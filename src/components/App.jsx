d4f79a3a8f2c5f2f55782b20b64a35d33410b549
Hanna Tubolets <116286387+HannaTubolets@users.noreply.github.com>
2023-02-14 06:03:02 +0300

    Updated App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,55 @@
 import { Component } from 'react';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { Statistics } from './Statistics/Statistics';
 import { Section } from './Section/Section';
-
+import { Notification } from './Notification/Notification';
 
 export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  countTotalFeedback = () => {
+    return Object.values(this.state).reduce((value, acc) => acc + value, 0);
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    return Math.round((this.state.good / this.countTotalFeedback()) * 100);
+  };
+
+  handleClick = option => {
+    this.setState(prevState => {
+      return {
+        [option]: prevState[option] + 1,
+      };
+    });
+  };
+
   render() {
+    const { good, neutral, bad } = this.state;
+
     return (
       <>
-        <Section title="Please leave feedback"/>
-        <FeedbackOptions />
+        <Section title="Please leave feedback" />
+        <FeedbackOptions
+          options={Object.keys(this.state)}
+          onLeaveFeedback={this.handleClick}
+        />
+        <Section title="Statistics">
+          {this.countTotalFeedback() > 0 ? (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={this.countTotalFeedback()}
+              positivePercentage={this.countPositiveFeedbackPercentage()}
+            />
+          ) : (
+            <Notification message="No feedback given" />
+          )}
+        </Section>
       </>
     );
   }
--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,54 +1,28 @@
-import { Component } from 'react';
-import { Button } from '../Button/Button';
-import { Statistics } from 'components/Statistics/Statistics';
-import { Section } from 'components/Section/Section';
-import { Notification } from 'components/Notification/Notification';
-// import css from './FeedbackOptions.module.css'
+import PropTypes from 'prop-types';
+import css from './FeedbackOptions.module.css';
 
-export class FeedbackOptions extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
-
-  countTotalFeedback = () => {
-    return Object.values(this.state).reduce((value, acc) => acc + value, 0);
-  };
-
-  countPositiveFeedbackPercentage = () => {
-    return Math.round((this.state.good / this.countTotalFeedback()) * 100);
-  };
-
-  handleClick = option => {
-    this.setState(prevState => {
-      return {
-        [option]: prevState[option] + 1,
-      };
-    });
-  };
-
-  render() {
-    const { good, neutral, bad } = this.state;
-    const options = Object.keys(this.state);
-
-    return (
-      <>
-        <Button options={options} onLeaveFeedback={this.handleClick} />
-        <Section title="Statistics">
-          {this.countTotalFeedback() > 0 ? (
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={this.countTotalFeedback()}
-              positivePercentage={this.countPositiveFeedbackPercentage()}
-            />
-          ) : (
-            <Notification message="No feedback given" />
-          )}
-        </Section>
-      </>
-    );
-  }
+export function FeedbackOptions({ options, onLeaveFeedback }) {
+  return (
+    <ul className={css.feedbackList}>
+      {options.map(option => {
+        return (
+          <li key={option}>
+            <button
+              className={css.btn}
+              type="button"
+              value={option}
+              onClick={() => onLeaveFeedback(option)}
+            >
+              {option}
+            </button>
+          </li>
+        );
+      })}
+    </ul>
+  );
 }
+
+FeedbackOptions.propTypes = {
+  onLeaveFeedback: PropTypes.func,
+  options: PropTypes.arrayOf(PropTypes.oneOf(['good', 'neutral', 'bad'])),
+};
